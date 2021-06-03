a232d70f8e0026b176ee3c1fec41b605675a7297
Jacob Piland <piland.jacob@gmail.com>
2021-06-03 10:13:28 -0500

    add tests for spacecraft and person

---
+++ spec/person_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require './lib/person'
+
+RSpec.describe Person do
+
+  it "exists" do
+
+    kathy = Person.new('Kathy Chan', 10)
+
+    expect(kathy).to be_a(Person)
+  end
+
+  it "has attributes" do
+
+    kathy = Person.new('Kathy Chan', 10)
+
+    expect(kathy.name).to eq('Kathy Chan')
+    expect(kathy.experience).to eq(10)
+    expect(kathy.specialties).to eq([])
+  end
+
+  it "can add specialty" do
+
+    kathy = Person.new('Kathy Chan', 10)
+
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+
+    expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+  end 
+end
